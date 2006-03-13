9f434d926d78e21da9bc2e932fce41d070faa351
broneill <broneill@d8c12fcc-1f0e-0410-bae1-d8fab76b7f20>
2006-03-13 06:07:14 +0000

    Added BasicConnection.

---
+++ src/dirmi/io/BasicConnection.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2006 Brian S O'Neill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package dirmi.io;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.net.Socket;
+
+/**
+ * 
+ *
+ * @author Brian S O'Neill
+ */
+public class BasicConnection implements Connection {
+    private final InputStream mIn;
+    private final OutputStream mOut;
+
+    public BasicConnection(InputStream in, OutputStream out) {
+        mIn = in;
+        mOut = out;
+    }
+
+    public BasicConnection(Socket socket) throws IOException {
+        mIn = socket.getInputStream();
+        mOut = socket.getOutputStream();
+    }
+
+    public InputStream getInputStream() {
+        return mIn;
+    }
+
+    public OutputStream getOutputStream() {
+        return mOut;
+    }
+
+    public void close() throws IOException {
+        mOut.close();
+        mIn.close();
+    }
+}
