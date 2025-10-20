6ccaeec43927023f8047103a6c3e36e456ec7df0
Brian S. O'Neill <bronee@gmail.com>
2025-10-20 09:21:54 -0700

    Update javadocs.

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.cojen.dirmi","l":"org.cojen.dirmi"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"org.cojen.dirmi","l":"org.cojen.dirmi"}];updateSearchResults();
